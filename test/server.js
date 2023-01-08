58c6ea6b82947e3128452e22045f519a7ea987a9
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2023-01-08 14:14:22 -0500

    Pass all data set by user on front side to back side

--- test/server.js
+++ test/server.js
@@ -1,18 +1,20 @@
 const fs = require("fs");
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = 3000;
 
-const card = process.argv[2] || "weight-conversion";
+const card = process.argv[2] || "examples/weight-conversion";
 
-const Front = fs.readFileSync(`./examples/${card}/Front.js`, "utf-8");
-const Back = fs.readFileSync(`./examples/${card}/Back.js`, "utf-8");
+// Read front and back user scripts
+const Front = fs.readFileSync(path.join(card, "Front.js"), "utf-8");
+const Back = fs.readFileSync(path.join(card, "Back.js"), "utf-8");
 
+// Prepare front and back sides
 const frontSide = fs
   .readFileSync("./model/frontTemplate.html", "utf-8")
   .replace("{{Front}}", Front)
   .replace("{{Back}}", Back);
-
 const backSide = fs
   .readFileSync("./model/backTemplate.html", "utf-8")
   .replace("{{FrontSide}}", frontSide)
@@ -29,22 +31,37 @@ app.get("/", (req, res) => {
 
 app.get("/front", (req, res) => {
   // Send user to back side and pass props
-  const script = `
+  const before = `
+    <script>
+      const keys = Object.keys(window);
+      </script>
+    `;
+  const after = `
     <button id="btn">Show back</button>
     <script>
-      function showBack() {
-        window.location.href = "/back?props=" + encodeURIComponent(JSON.stringify(window.props));
-      }
-      document.getElementById("btn").addEventListener("click", showBack);
+      const data = {};
+      Object.entries(window).forEach(([key, value]) => {
+        if (!keys.includes(key)) {
+          data[key] = value;
+        }
+      });
+      document.getElementById("btn").addEventListener("click", () => {
+        window.location.href = "/back?data=" + encodeURIComponent(JSON.stringify(data));
+      });
     </script>
   `;
-  const page = cardTemplate.replace("{{Card}}", frontSide + script);
+  const page = cardTemplate.replace("{{Card}}", before + frontSide + after);
   res.send(page);
 });
 
 app.get("/back", (req, res) => {
   // Render back side with props passed from front side
-  const script = `<script>window.props = ${req.query.props}</script>`;
+  const script = `<script>
+    const data = JSON.parse(decodeURIComponent('${req.query.data}'));
+    Object.entries(data).forEach(([key, value]) => {
+      window[key] = value;
+    });
+    </script>`;
   const page = cardTemplate.replace("{{Card}}", script + backSide);
   res.send(page);
 });
